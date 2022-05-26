9ae3409c2b66be042accf0e0d01ca8a3863e46a9
izzannazzi <izzannazzi10@gmail.com>
2022-05-27 00:17:35 +0700

    part 2 ended

--- function.php
+++ function.php
@@ -29,4 +29,28 @@ if (isset($_POST['login'])){
         </script>';
     }
 }
+
+if (isset($_POST['tambahproduk'])) {
+    //deskripsi initial variable
+    $nama_produk = $_POST['nama_produk'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga = $_POST['harga'];
+    $stock = $_POST['stock'];
+
+    $insert_produk = mysqli_query($koneksi,"INSERT INTO produk (nama_produk, deskripsi, harga, stock) VALUES ('$nama_produk',
+    '$deskripsi', '$harga', '$stock')");
+
+    if($insert_produk){
+        header('location:stock.php');
+    } else {
+        echo '
+        <script>
+
+        alert("Gagal Tambah Produk")
+        window.location.href="stock.php"
+
+        </script>';
+    }
+}
+
 ?>
